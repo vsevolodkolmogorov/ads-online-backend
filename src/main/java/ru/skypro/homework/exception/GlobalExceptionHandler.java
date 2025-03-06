30b0c9db864266b315fca8b18c1ecbaa04b9b58b
vilar <loadslowly@gmail.com>
2025-03-06 22:51:10 +0500

    documentation: написал JavaDoc-комментарии для обработчика глобальной ошибки

--- src/main/java/ru/skypro/homework/exception/GlobalExceptionHandler.java
+++ src/main/java/ru/skypro/homework/exception/GlobalExceptionHandler.java
@@ -5,6 +5,15 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+/**
+ * Глобальный обработчик исключений для обработки различных ошибок в приложении.
+ *
+ * Этот класс перехватывает исключения типа {@link RuntimeException} и возвращает соответствующие ответы.
+ * В случае ошибки с сообщением "Access denied" возвращается статус HTTP 403 (FORBIDDEN).
+ * Для других исключений типа {@link RuntimeException} возвращается статус HTTP 500 (INTERNAL_SERVER_ERROR).
+ *
+ * Методы этого класса используют аннотацию {@link ExceptionHandler} для перехвата исключений и возврата корректных HTTP-ответов.
+ */
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
