15ecdfac56bfcb160471876d5b700d11c5d826b8
vilar <loadslowly@gmail.com>
2025-03-06 22:48:49 +0500

    documentation: написал JavaDoc-комментарии для всех интерфейсов сервисов

--- src/main/java/ru/skypro/homework/service/CommentService.java
+++ src/main/java/ru/skypro/homework/service/CommentService.java
@@ -5,17 +5,33 @@ import ru.skypro.homework.dto.CommentsDTO;
 import ru.skypro.homework.dto.CreateOrUpdateCommentDTO;
 import ru.skypro.homework.model.Comment;
 
+/**
+ * Сервисный интерфейс для работы с комментариями.
+ *
+ * Интерфейс предоставляет методы для выполнения операций с комментариями:
+ * - Преобразование сущности {@link Comment} в {@link CommentDTO} {@link #getCommentDTO(Comment)}.
+ * - Обновление существующего комментария {@link #updateComment(Integer, CreateOrUpdateCommentDTO)}.
+ * - Добавление нового комментария к объявлению {@link #addComment(Integer, CreateOrUpdateCommentDTO)}.
+ * - Получение списка комментариев для объявления {@link #getComments(Integer)}.
+ * - Создание сущности {@link Comment} на основе данных {@link CommentDTO} {@link #createCommentFromDTO(CommentDTO)}.
+ * - Проверка, является ли пользователь автором комментария {@link #isCommentAuthor(Integer, String)}.
+ * - Удаление комментария {@link #deleteComment(Integer)}.
+ *
+ * Методы интерфейса возвращают объекты DTO, такие как {@link CommentDTO} и {@link CommentsDTO}, а также выполняют операции обновления и получения данных.
+ */
 public interface CommentService {
-    CommentDTO getCommentDTO (Comment comment);
-    Comment createCommentFromDTO (CommentDTO commentDTO);
 
-    CommentDTO addComment(Integer adId, CreateOrUpdateCommentDTO commentDTO);
-
-    void deleteComment(Integer commentId);
-
-    CommentsDTO getComments(Integer adId);
+    CommentDTO getCommentDTO(Comment comment);
 
     CommentDTO updateComment(Integer commentId, CreateOrUpdateCommentDTO commentDTO);
 
+    CommentDTO addComment(Integer adId, CreateOrUpdateCommentDTO commentDTO);
+
+    CommentsDTO getComments(Integer adId);
+
+    Comment createCommentFromDTO(CommentDTO commentDTO);
+
     boolean isCommentAuthor(Integer commentId, String username);
+
+    void deleteComment(Integer commentId);
 }
